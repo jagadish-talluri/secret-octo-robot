e3f97ed1265826b0001d2fc27cde906a068cf164
Jagadish Talluri <jagadish-talluri@users.noreply.github.com>
2015-11-27 21:55:40 +0530

    Date Format, Int, Float validations added

--- rough-mapper-1.java
+++ rough-mapper-1.java
@@ -1,8 +1,11 @@
 package a.b.c.tsa.validation;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -26,12 +29,13 @@ public class RecordValidationMapper extends Mapper<LongWritable, Text, Text, Nul
 
         private int nColumnCount;
         private NodeList nList;
-        private String delimiter;
+        private String delimiter, dateFormat;
 
         // create arraylist
         private ArrayList<String> fieldLengths = new ArrayList<String>();
         private ArrayList<String> isNullableList = new ArrayList<String>();
         private ArrayList<String> columnNames = new ArrayList<String>();
+        private ArrayList<String> dataTypes = new ArrayList<String>();
 
         private MultipleOutputs<Text, Text> mos;
 
@@ -76,6 +80,10 @@ public class RecordValidationMapper extends Mapper<LongWritable, Text, Text, Nul
                     	NodeList dNodeList = doc.getElementsByTagName("delimiter");
                     	Node delimiterNode = dNodeList.item(0);
                     	delimiter = delimiterNode.getTextContent();
+                    	
+                    	NodeList dfNodeList = doc.getElementsByTagName("dateformat");
+                    	Node dfNode = dfNodeList.item(0);
+                    	dateFormat = dfNode.getTextContent();
 
                         
                         System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
@@ -100,24 +108,59 @@ public class RecordValidationMapper extends Mapper<LongWritable, Text, Text, Nul
                                         fieldLengths.add(eElement.getElementsByTagName("datalength").item(0).getTextContent());
                                         isNullableList.add(eElement.getElementsByTagName("isnullable").item(0).getTextContent());
                                         columnNames.add(eElement.getElementsByTagName("columnname").item(0).getTextContent());
-
+                                        dataTypes.add(eElement.getElementsByTagName("sourcedatatype").item(0).getTextContent());
                                 }
                         }
                 } catch (Exception ioe) {
                         System.err.println("Exception while reading xml conf file '" + xmlConfFile + "' : " + ioe.toString());
                 }
         }
-
+        
+		public boolean isDateValid(String dateToValidate, String dateFromat) {
+			SimpleDateFormat sdf = new SimpleDateFormat(dateFromat);
+			sdf.setLenient(false);
+	
+			try {
+	
+				// if not valid, it will throw ParseException
+				Date date = sdf.parse(dateToValidate);
+				//System.out.println(date);
+	
+			} catch (ParseException e) {
+	
+				e.printStackTrace();
+				return false;
+			}
+	
+			return true;
+		}
+    	
+		public boolean isDataTypeValid(String dataToValiate, String dataType){
+			
+			if (dataType.equals("int")){
+				try{int i = Integer.parseInt(dataToValiate);}
+				catch(Exception e) {return false;}
+			} else if (dataType.equals("float")){
+				try{float f = Float.parseFloat(dataToValiate);}
+				catch(Exception e) {return false;}
+				
+			}
+			
+			return true;
+		}
+		
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
                 String errorMsg = " [Error Msg";
                 String line = value.toString();
                 
+                
                 /* string tokenizer failed to identify empty tokens */
                 //StringTokenizer tokenizer = new StringTokenizer(line, ",");
                 //int nTokenCount = tokenizer.countTokens();
 
-                // replace StringTokenizer logic with String splits in a List, this helps not to change the while loop, -1 for including trailing empty tokens
+                // replace StringTokenizer logic with String splits to add empty token recognition 
+                // string splits in a List, this helps not to change the while loop, -1 for including trailing empty tokens
                 String[] lineSplits = line.split(delimiter, -1);
                 int nTokenCount = lineSplits.length;
                 List<String> lineSplitsList = Arrays.asList(lineSplits);
@@ -127,47 +170,71 @@ public class RecordValidationMapper extends Mapper<LongWritable, Text, Text, Nul
                 Iterator itrFieldLen = fieldLengths.iterator();
                 Iterator itrNullable = isNullableList.iterator();
                 Iterator itrColNames = columnNames.iterator();
+                Iterator itrDataTypes = dataTypes.iterator();
 
 //              System.out.println("------------token-count----------------" + nTokenCount);
 
                 boolean isValid = false;
                 
-                // column count validation
+                // column count validation, delimiter check
                 if (nColumnCount == nTokenCount) {
                         isValid = true;
                         // stop iteration if any column validation fails, performance tip
                         while (isValid && tokenizer.hasNext()) {
-
+                        		
+                        		// these are from input file
                                 String token = tokenizer.next().toString().trim();
+                                
+                                // these are from xml conf file
                                 int fieldLength = Integer.parseInt(itrFieldLen.next().toString());
                                 String isNullableValue = itrNullable.next().toString();
                                 String colName = itrColNames.next().toString();
-
+                                String DataType = itrDataTypes.next().toString();
+                                
                                 // check for validation of NULLs, is it NULL? and can it be NULL?
                                 if (token.length() == 0 & isNullableValue.equals("N")) {
                                         isValid = false;
                                         errorMsg = errorMsg.concat(" : \'"+colName+"\' Field is Null");
                                 }
 
-                                // field length validation
-                                else if (fieldLength != token.length()) {
-                                        isValid = false;
-                                        errorMsg = errorMsg.concat(" : \'"+colName+"\' Field expected length is "+fieldLength+" but received "+token.length()) ;
+                                // field length validation, is the expected length same?
+//                                else if (fieldLength != token.length()) {
+//                                        isValid = false;
+//                                        errorMsg = errorMsg.concat(" : \'"+colName+"\' Field expected length is \'"+fieldLength+"\' but received \'"+token.length()+"\'") ;
+//                                }
+                                
+                                // date format validation, is the field datetime? is it valid?
+                                else if (DataType.equals("datetime") & (!isDateValid(token,dateFormat))){
+                                	isValid = false;
+                                	errorMsg = errorMsg.concat(" : \'"+colName+"\' Field expected date format is \'"+dateFormat+"\' but received \'"+token+"\'");
                                 }
+                                
+                                // integer data type validation, is the field int? is it valid?
+                                else if (DataType.equals("int") & (!isDataTypeValid(token, "int"))){
+                                	isValid = false;
+                                	errorMsg = errorMsg.concat(" : unable to pasre as Integer, received \'"+token+"\'");
+                                }
+
+                                // float data type validation, is the field float? is it valid?
+                                else if (DataType.equals("float") & (!isDataTypeValid(token, "float"))){
+                                	isValid = false;
+                                	errorMsg = errorMsg.concat(" : unable to pasre as Float, received \'"+token+"\'");
+                                }
+                                
                         }
-                } else {errorMsg = errorMsg.concat(" : either delimiter issue or column count doesn't match, expected "+nColumnCount+" but received "+nTokenCount);}
+                } else {errorMsg = errorMsg.concat(" : either delimiter is wrong or column count doesn't match, expected \'"+nColumnCount+"\' but received \'"+nTokenCount+"\'");}
                 
-                
+                // write the output to files
                 if (isValid) {
                         context.getCounter(RecordValidation.COUNTERS.VALID_RECORDS).increment(1L);
                         mos.write("validrecords", NullWritable.get(), value);
 
                 } else {
                         context.getCounter(RecordValidation.COUNTERS.INVALID_RECORDS).increment(1L);
-                        String r = errorMsg.toString()+"]";
-                        Text t = new Text(value.toString()+r);
-                        //value = new Text(value.toString() + errorMsg);
-                        mos.write("invalidrecords", NullWritable.get(), t);
+//                        String r = errorMsg.toString()+"]";
+//                        Text t = new Text(value.toString()+r);
+                        value = new Text(value.toString() + errorMsg.toString() + "]");
+                        mos.write("invalidrecords", NullWritable.get(), value);
                 }
         }
 
